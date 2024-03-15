6b765027d519bede8b05be2eee45981b39382c37
fablnt <fabio.lentini@mail.polimi.it>
2024-03-15 15:34:07 +0000

    Parameters implementation

--- src/FisherKolmogorov.hpp
+++ src/FisherKolmogorov.hpp
@@ -12,57 +12,103 @@
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/tria.h>
 
-//Two header taken by reading documentation in order to obatin finite element object
+// Two header taken by reading documentation in order to obatin finite element object
 #include <deal.II/lac/petsc_vector.h>
 #include <deal.II/lac/petsc_sparse_matrix.h>
 
-//Header taken in order to obatin ConditionalOStream type
+// Header taken in order to obatin ConditionalOStream type
 #include <deal.II/base/conditional_ostream.h>
 
 #include <fstream>
 #include <iostream>
 #include <filesystem>
 
-
 using namespace dealii;
 
-class FisherKolmogorov {
+class FisherKolmogorov
+{
 
-    public:
+public:
+  // Physical dimension (1D, 3D)
+  static constexpr unsigned int dim = 1;
+  class SpreadingCoefficient : public Function<dim>
+  {
+  public:
+    SpreadingCoefficient()
+    {
+    }
 
-    // Physical dimension (1D, 3D)
-    static constexpr unsigned int dim = 3;
-    
-    FisherKolmogorov(const std::string &mesh_file_name_, const unsigned int &r_)
-    : mesh_file_name(mesh_file_name_)
-    , r(r_)
-    , mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD))
-    , mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD))
-    , mesh(MPI_COMM_WORLD)
-    , pcout(std::cout, mpi_rank == 0)
-  {}
+    virtual double
+    value(const Point<dim> & /*p*/, const unsigned int /*component */ = 0) const override
+    {
+      return 0.0001;
+    }
+  };
 
-   void setup();
-   
-   protected:
-   // Path to the mesh file.
-   const std::string mesh_file_name;
-   
-   // Polynomial degree.
-   const unsigned int r;
-   
-   // Number of MPI processes.
-   const unsigned int mpi_size;
-   
-   // This MPI process.
-   const unsigned int mpi_rank;
+  class FunctionC0 : public Function<dim>
+  {
+  public:
+    FunctionC0()
+    {
+    }
+
+    virtual double
+    value(const Point<dim> &p, const unsigned int /*component*/ = 0) const override
+    {
+      if (p[0] == 0)
+        return 0.1;
+      else
+        return 0.0;
+    }
+  };
+
+  FisherKolmogorov(const std::string &mesh_file_name_,
+                   const unsigned int &r_,
+                   const double &deltat_,
+                   const double &T_)
+      : mpi_size(Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD)), mpi_rank(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)), pcout(std::cout, mpi_rank == 0), T(T_), mesh_file_name(mesh_file_name_), r(r_), deltat(deltat_), mesh(MPI_COMM_WORLD)
+  {
+  }
+
+  void setup();
+
+protected:
+  // Number of MPI processes.
+  const unsigned int mpi_size;
+
+  // This MPI process.
+  const unsigned int mpi_rank;
+
+  // Parallel output stream.
+  ConditionalOStream pcout;
+
+  // Coefficient alpha
+  const double growth_coefficient = 1;
+
+  // Coefficient D
+  SpreadingCoefficient spreading_coefficient;
+
+  // Initial concentration c(t = 0)
+  FunctionC0 c_0;
+
+  double time;
+
+  const double T;
+
+  // Path to the mesh file.
+  const std::string mesh_file_name;
+
+  // Polynomial degree.
+  const unsigned int r;
+
+  const double deltat;
 
   // Triangulation. The parallel::fullydistributed::Triangulation class manages
   // a triangulation that is completely distributed (i.e. each process only
   // knows about the elements it owns and its ghost elements).
   parallel::fullydistributed::Triangulation<dim> mesh;
 
-    // Finite element space.
+  // Finite element space.
   std::unique_ptr<FiniteElement<dim>> fe;
 
   // Quadrature formula.
@@ -71,11 +117,6 @@ class FisherKolmogorov {
   // DoF handler.
   DoFHandler<dim> dof_handler;
 
-  // Parallel output stream.
-  ConditionalOStream pcout;
-  
   // DoFs owned by current process.
   IndexSet locally_owned_dofs;
-
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,10 @@ int main (int argc, char *argv[]) {
 
     const std::string  mesh_filename = "../mesh/brain-h3.0.msh";
     const unsigned int degree        = 1;
+    const double       deltat        = 0.1;
+    const double       T             = 20;
 
-    FisherKolmogorov problem(mesh_filename, degree);
+    FisherKolmogorov problem(mesh_filename, degree, deltat, T);
     
 }
 
