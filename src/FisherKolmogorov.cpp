a3e0616ba7fb9660f7b54f15cb5e184c0816da60
fablnt <fabio.lentini@mail.polimi.it>
2024-05-31 11:19:11 +0000

    spreading coefficient computation in assembly

--- src/FisherKolmogorov.cpp
+++ src/FisherKolmogorov.cpp
@@ -190,240 +190,242 @@ FisherKolmogorov::assemble_system()
 
       for (unsigned int q = 0; q < n_q; ++q)
         {
-          if (cell->material_id() == 1)
+          for (unsigned int q = 0; q < n_q; ++q)
             {
-              growth_coefficient_loc =
-                growth_coefficient_grey.value(fe_values.quadrature_point(q));
-
-              spreading_coefficient_loc =
-                spreading_coefficient.value(isotropic_coefficient_grey.value(
-                                              fe_values.quadrature_point(q)),
-                                            anisotropic_coefficient_grey.value(
-                                              fe_values.quadrature_point(q)),
-                                            direction);
-            }
-
-          else
-            {
-              growth_coefficient_loc =
-                growth_coefficient_white.value(fe_values.quadrature_point(q));
-
-              spreading_coefficient_loc =
-                spreading_coefficient.value(isotropic_coefficient_white.value(
-                                              fe_values.quadrature_point(q)),
-                                            anisotropic_coefficient_white.value(
-                                              fe_values.quadrature_point(q)),
-                                            direction);
-            }
-
-          for (unsigned int i = 0; i < dofs_per_cell; ++i)
-            {
-              for (unsigned int j = 0; j < dofs_per_cell; ++j)
+              if (cell->material_id() == 1)
                 {
-                  // Mass matrix.
-                  cell_matrix(i, j) +=
-                    fe_values.shape_value(i, q) * // time derivative term
-                    fe_values.shape_value(j, q) / deltat * fe_values.JxW(q);
+                  growth_coefficient_loc = growth_coefficient_grey.value(
+                    fe_values.quadrature_point(q));
 
-                  // Non-linear stiffness matrix, first term.
-                  cell_matrix(i, j) +=
-                    scalar_product(spreading_coefficient_loc *
-                                     fe_values.shape_grad(i, q),
-                                   fe_values.shape_grad(j, q)) *
-                    fe_values.JxW(q);
-
-                  // Non-linear stiffness matrix, second term.
-                  cell_matrix(i, j) -=
-                    growth_coefficient_loc * fe_values.shape_value(i, q) *
-                    fe_values.shape_value(j, q) * fe_values.JxW(q);
-
-                  // Non-linear stiffness matrix, third term.
-                  cell_matrix(i, j) +=
-                    2 * growth_coefficient_loc * solution_loc[q] *
-                    fe_values.shape_value(i, q) * fe_values.shape_value(j, q) *
-                    fe_values.JxW(q);
+                  spreading_coefficient_loc =
+                    isotropic_coefficient_grey.value(
+                      fe_values.quadrature_point(q)) *
+                    unit_symmetric_tensor<dim>() *
+                    anisotropic_coefficient_grey.value(
+                      fe_values.quadrature_point(q)) *
+                    outer_product(direction, direction);
                 }
 
-              // Assemble the residual vector (with changed sign).
-              // Time derivative term.
-              cell_residual(i) -= (solution_loc[q] - solution_old_loc[q]) /
-                                  deltat * fe_values.shape_value(i, q) *
-                                  fe_values.JxW(q);
+              else
+                {
+                  growth_coefficient_loc = growth_coefficient_white.value(
+                    fe_values.quadrature_point(q));
 
-              // Diffusion term
-              cell_residual(i) -= scalar_product(spreading_coefficient_loc *
-                                                   solution_gradient_loc[q],
-                                                 fe_values.shape_grad(i, q)) *
-                                  fe_values.JxW(q);
+                  spreading_coefficient_loc =
+                    isotropic_coefficient_white.value(
+                      fe_values.quadrature_point(q)) *
+                    unit_symmetric_tensor<dim>() *
+                    anisotropic_coefficient_white.value(
+                      fe_values.quadrature_point(q)) *
+                    outer_product(direction, direction);
+                }
 
-              // Growth term
-              cell_residual(i) += ((growth_coefficient_loc * solution_loc[q]) *
-                                   (1 - solution_loc[q])) *
-                                  fe_values.shape_value(i, q) *
-                                  fe_values.JxW(q);
+              for (unsigned int i = 0; i < dofs_per_cell; ++i)
+                {
+                  for (unsigned int j = 0; j < dofs_per_cell; ++j)
+                    {
+                      // Mass matrix.
+                      cell_matrix(i, j) +=
+                        fe_values.shape_value(i, q) * // time derivative term
+                        fe_values.shape_value(j, q) / deltat * fe_values.JxW(q);
+
+                      // Non-linear stiffness matrix, first term.
+                      cell_matrix(i, j) +=
+                        scalar_product(spreading_coefficient_loc *
+                                         fe_values.shape_grad(i, q),
+                                       fe_values.shape_grad(j, q)) *
+                        fe_values.JxW(q);
+
+                      // Non-linear stiffness matrix, second term.
+                      cell_matrix(i, j) -=
+                        growth_coefficient_loc * fe_values.shape_value(i, q) *
+                        fe_values.shape_value(j, q) * fe_values.JxW(q);
+
+                      // Non-linear stiffness matrix, third term.
+                      cell_matrix(i, j) +=
+                        2 * growth_coefficient_loc * solution_loc[q] *
+                        fe_values.shape_value(i, q) *
+                        fe_values.shape_value(j, q) * fe_values.JxW(q);
+                    }
+
+                  // Assemble the residual vector (with changed sign).
+                  // Time derivative term.
+                  cell_residual(i) -= (solution_loc[q] - solution_old_loc[q]) /
+                                      deltat * fe_values.shape_value(i, q) *
+                                      fe_values.JxW(q);
+
+                  // Diffusion term
+                  cell_residual(i) -=
+                    scalar_product(spreading_coefficient_loc *
+                                     solution_gradient_loc[q],
+                                   fe_values.shape_grad(i, q)) *
+                    fe_values.JxW(q);
+
+                  // Growth term
+                  cell_residual(i) +=
+                    ((growth_coefficient_loc * solution_loc[q]) *
+                     (1 - solution_loc[q])) *
+                    fe_values.shape_value(i, q) * fe_values.JxW(q);
 
 
-              // Forcing term equals to 0, no contribution to cell_residual(i)
+                  // Forcing term equals to 0, no contribution to
+                  // cell_residual(i)
+                }
+
+              cell->get_dof_indices(dof_indices);
+
+              jacobian_matrix.add(dof_indices, cell_matrix);
+              residual_vector.add(dof_indices, cell_residual);
             }
 
-          cell->get_dof_indices(dof_indices);
-
-          jacobian_matrix.add(dof_indices, cell_matrix);
-          residual_vector.add(dof_indices, cell_residual);
+          // Parallel communication among processes
+          jacobian_matrix.compress(VectorOperation::add);
+          residual_vector.compress(VectorOperation::add);
         }
-
-      // Parallel communication among processes
-      jacobian_matrix.compress(VectorOperation::add);
-      residual_vector.compress(VectorOperation::add);
     }
-}
 
-/**
- * Solves the linear system obtained in the assemble_system() function,
- * parallelizing the computation on the different active MPI processes.
- * Uses the GMRES method as iterative method, setting the maximum number of
- * iterations equals to 1000, the tolerance equals to 1.e-6 and the Algebraic
- * Multigrid as preconditioner.
- */
-void
-FisherKolmogorov::solve_linear_system()
-{
-  // setting for solver
-  SolverControl solver_control(
-    1000, 1e-6 * residual_vector.l2_norm()); // residual norm taken into account
-                                             // for a more reliable tolerance
-
-  // GMRES solver
-  SolverGMRES<TrilinosWrappers::MPI::Vector> solver(solver_control);
-
-  // AMG preconditioner declaration and initialization
-  TrilinosWrappers::PreconditionAMG preconditioner;
-  preconditioner.initialize(
-    jacobian_matrix, TrilinosWrappers::PreconditionAMG::AdditionalData(1.0));
-
-  // solve with GMRES solver
-  solver.solve(jacobian_matrix, delta_owned, residual_vector, preconditioner);
-  pcout << "  " << solver_control.last_step() << " CG iterations" << std::endl;
-}
-
-/**
- * Solves the non-linear problem using the Newton method.
- * Invoked for each time step by the solve() function, finds a numerical
- * solution of the non-linear system assembling and solving a linear system,
- * using as maxinum number of iterations and as tolerance the same used for the
- * GMRES solver.
- */
-void
-FisherKolmogorov::solve_newton()
-{
-  // parameters for the GMRES solver
-  const unsigned int n_max_iters        = 1000;
-  const double       residual_tolerance = 1e-6;
-  unsigned int       n_iter             = 0;
-  double             residual_norm      = residual_tolerance + 1;
-
-  // There are no dirichlet boundary conditions
-
-
-  // iterative cycle till convergence
-  while (n_iter < n_max_iters && residual_norm > residual_tolerance)
-    {
-      // assembly of the jacobian matrix and the residual vector
-      assemble_system();
-      residual_norm = residual_vector.l2_norm();
-
-      pcout << "  Newton iteration " << n_iter << "/" << n_max_iters
-            << " - ||r|| = " << std::scientific << std::setprecision(6)
-            << residual_norm << std::flush;
-
-      // We actually solve the system only if the residual is larger than the
-      // tolerance.
-      if (residual_norm > residual_tolerance)
-        {
-          solve_linear_system();
-
-          solution_owned += delta_owned;
-          solution = solution_owned;
-        }
-      else
-        {
-          pcout << " < tolerance" << std::endl;
-        }
-
-      ++n_iter;
-    }
-}
-
-/**
- * Function called by main() function.
- * For each time step applies the Newton method (solving the tangent problem)
- * and calls the output() function which creates output files.
- */
-void
-FisherKolmogorov::solve()
-{
-  pcout << "===============================================" << std::endl;
-
-  time = 0.0;
-
-  // Apply the initial condition.
+  /**
+   * Solves the linear system obtained in the assemble_system() function,
+   * parallelizing the computation on the different active MPI processes.
+   * Uses the GMRES method as iterative method, setting the maximum number of
+   * iterations equals to 1000, the tolerance equals to 1.e-6 and the Algebraic
+   * Multigrid as preconditioner.
+   */
+  void FisherKolmogorov::solve_linear_system()
   {
-    pcout << "Applying the initial condition" << std::endl;
+    // setting for solver
+    SolverControl solver_control(
+      1000,
+      1e-6 * residual_vector.l2_norm()); // residual norm taken into account
+                                         // for a more reliable tolerance
 
-    VectorTools::interpolate(dof_handler, c_0, solution_owned);
-    solution = solution_owned;
+    // GMRES solver
+    SolverGMRES<TrilinosWrappers::MPI::Vector> solver(solver_control);
 
-    // Output the initial solution.
-    output(0);
-    pcout << "-----------------------------------------------" << std::endl;
+    // AMG preconditioner declaration and initialization
+    TrilinosWrappers::PreconditionAMG preconditioner;
+    preconditioner.initialize(
+      jacobian_matrix, TrilinosWrappers::PreconditionAMG::AdditionalData(1.0));
+
+    // solve with GMRES solver
+    solver.solve(jacobian_matrix, delta_owned, residual_vector, preconditioner);
+    pcout << "  " << solver_control.last_step() << " CG iterations"
+          << std::endl;
   }
 
-  unsigned int time_step = 0;
+  /**
+   * Solves the non-linear problem using the Newton method.
+   * Invoked for each time step by the solve() function, finds a numerical
+   * solution of the non-linear system assembling and solving a linear system,
+   * using as maxinum number of iterations and as tolerance the same used for
+   * the GMRES solver.
+   */
+  void FisherKolmogorov::solve_newton()
+  {
+    // parameters for the GMRES solver
+    const unsigned int n_max_iters        = 1000;
+    const double       residual_tolerance = 1e-6;
+    unsigned int       n_iter             = 0;
+    double             residual_norm      = residual_tolerance + 1;
 
-  while (
-    time <
-    T -
-      0.5 *
-        deltat) // - 0.5*deltat is a tolerance setted to avoid extra-iterations
+    // There are no dirichlet boundary conditions
+
+
+    // iterative cycle till convergence
+    while (n_iter < n_max_iters && residual_norm > residual_tolerance)
+      {
+        // assembly of the jacobian matrix and the residual vector
+        assemble_system();
+        residual_norm = residual_vector.l2_norm();
+
+        pcout << "  Newton iteration " << n_iter << "/" << n_max_iters
+              << " - ||r|| = " << std::scientific << std::setprecision(6)
+              << residual_norm << std::flush;
+
+        // We actually solve the system only if the residual is larger than the
+        // tolerance.
+        if (residual_norm > residual_tolerance)
+          {
+            solve_linear_system();
+
+            solution_owned += delta_owned;
+            solution = solution_owned;
+          }
+        else
+          {
+            pcout << " < tolerance" << std::endl;
+          }
+
+        ++n_iter;
+      }
+  }
+
+  /**
+   * Function called by main() function.
+   * For each time step applies the Newton method (solving the tangent problem)
+   * and calls the output() function which creates output files.
+   */
+  void FisherKolmogorov::solve()
+  {
+    pcout << "===============================================" << std::endl;
+
+    time = 0.0;
+
+    // Apply the initial condition.
     {
-      time += deltat;
-      ++time_step;
+      pcout << "Applying the initial condition" << std::endl;
 
-      // Store the old solution, so that it is available for assembly.
-      solution_old = solution;
+      VectorTools::interpolate(dof_handler, c_0, solution_owned);
+      solution = solution_owned;
 
-      pcout << "n = " << std::setw(3) << time_step << ", t = " << std::setw(5)
-            << std::fixed << time << std::endl;
-
-      // At every time step Newton's method to solve the non-linear system
-      solve_newton();
-
-      output(time_step);
-
-      pcout << std::endl;
+      // Output the initial solution.
+      output(0);
+      pcout << "-----------------------------------------------" << std::endl;
     }
-}
 
-/**
- * Output function.
- * Creates the output data for the selected time step, in particular a VTU file
- * for each MPI process, a PVTU file that points to VTU files and contains
- * information about splitting data across multiple MPI processes.
- * @param time_step   represents a distinct instant within the time interval T
- */
-void
-FisherKolmogorov::output(const unsigned int &time_step) const
-{
-  DataOut<dim> data_out;
-  data_out.add_data_vector(dof_handler, solution, "u");
+    unsigned int time_step = 0;
 
-  std::vector<unsigned int> partition_int(mesh.n_active_cells());
-  GridTools::get_subdomain_association(mesh, partition_int);
-  const Vector<double> partitioning(partition_int.begin(), partition_int.end());
-  data_out.add_data_vector(partitioning, "partitioning");
+    while (time < T - 0.5 * deltat) // - 0.5*deltat is a tolerance setted to
+                                    // avoid extra-iterations
+      {
+        time += deltat;
+        ++time_step;
 
-  data_out.build_patches();
+        // Store the old solution, so that it is available for assembly.
+        solution_old = solution;
 
-  data_out.write_vtu_with_pvtu_record(
-    "./", "output", time_step, MPI_COMM_WORLD, 3);
-}
+        pcout << "n = " << std::setw(3) << time_step << ", t = " << std::setw(5)
+              << std::fixed << time << std::endl;
+
+        // At every time step Newton's method to solve the non-linear system
+        solve_newton();
+
+        output(time_step);
+
+        pcout << std::endl;
+      }
+  }
+
+  /**
+   * Output function.
+   * Creates the output data for the selected time step, in particular a VTU
+   * file for each MPI process, a PVTU file that points to VTU files and
+   * contains information about splitting data across multiple MPI processes.
+   * @param time_step   represents a distinct instant within the time interval T
+   */
+  void FisherKolmogorov::output(const unsigned int &time_step) const
+  {
+    DataOut<dim> data_out;
+    data_out.add_data_vector(dof_handler, solution, "u");
+
+    std::vector<unsigned int> partition_int(mesh.n_active_cells());
+    GridTools::get_subdomain_association(mesh, partition_int);
+    const Vector<double> partitioning(partition_int.begin(),
+                                      partition_int.end());
+    data_out.add_data_vector(partitioning, "partitioning");
+
+    data_out.build_patches();
+
+    data_out.write_vtu_with_pvtu_record(
+      "./", "output", time_step, MPI_COMM_WORLD, 3);
+  }
