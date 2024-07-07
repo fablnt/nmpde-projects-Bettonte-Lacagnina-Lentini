090c0a3d325c84d003d5c10c4d96b1b99b286011
fablnt <fabio.lentini@mail.polimi.it>
2024-07-07 17:10:48 +0000

    main parameters

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@ main(int argc, char *argv[])
 {
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const std::string  mesh_filename  = "../mesh/ellipse.msh";
+  const std::string  mesh_filename  = "../mesh/brain-h3.0.msh";
   const unsigned int degree         = 1;
   const double       deltat         = 0.4;
   const double       T              = 40;
-  const std::string  seeding_region = "Amyloid-Beta deposits";
+  const std::string  seeding_region = "Tau inclusions";
   const std::string  orientation    = "axon-based";
 
   FisherKolmogorov problem(
